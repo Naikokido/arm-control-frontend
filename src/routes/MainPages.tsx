b3fd7eea5fca7c6e2afbb18c68996838c1756009
naiko <nicolasbarrera.jpg@gmail.com>
2024-09-23 13:23:27 -0300

    Nuevas pages, de control de robot About, Contact, dashboard, home. Se modifico MainPages, hay que modificar las nuevas pagees pues estan con css tienen que esta con Tailwind

--- src/routes/MainPages.tsx
+++ src/routes/MainPages.tsx
@@ -1,5 +1,9 @@
 import { LoginPage } from "../pages/LoginPage";
 import type { routerType } from "./routerTypes";
+import About from "../pages/About"
+import Contact from "../pages/Contact";
+import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
 // import Error404 from '../../presentation/pages/404.tsx';
 
 const MainPages: routerType[] = [
@@ -13,6 +17,27 @@ const MainPages: routerType[] = [
     element: <LoginPage />,
     title: "Login",
   },
+  {
+    path: "home",
+    element: <Home/>,
+    title: "Home",
+  },
+  {
+    path: "about",
+    element: <About/>,
+    title: "About",
+  },
+  {
+    path: "contact",
+    element: <Contact/>,
+    title: "Contact",
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard/>,
+    title: "Dashboard",
+  },
+  
 ];
 
 export default MainPages;
